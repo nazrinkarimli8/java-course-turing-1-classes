64cfc374afd2c2221d1f02755dceb8fc3a143499
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-01-22 20:53:10 +0400

    first commit

---
+++ src/main/java/az/edu/turing/MultiplicationApp.java
@@ -0,0 +1,8 @@
+package az.edu.turing;
+public class MultiplicationApp {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("2*" + i + "=" + 2 * i);
+        }
+    }
+}
