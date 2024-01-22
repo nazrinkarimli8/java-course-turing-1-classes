36e9be832c6330fd26e242d10fec8673b56804bd
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-01-22 20:29:42 +0400

    first commit

---
+++ src/main/java/az/edu/turing/StudentScor.java
@@ -0,0 +1,37 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class StudentScor {
+    Scanner sc = new Scanner(System.in);
+    double grade = sc.nextInt();
+    if(grade>0&&grade<51)
+
+    {
+        System.out.println("F");
+    } else if(grade>=51&&grade<61)
+
+    {
+        System.out.println("E");
+    }else if(grade>=61&&grade<71)
+
+    {
+        System.out.println("D");
+    }else if(grade>=71&&grade<81)
+
+    {
+        System.out.println("C");
+    }else if(grade>=81&&grade<91)
+
+    {
+        System.out.println("B");
+    }else if(grade>=91&&grade<=100)
+
+    {
+        System.out.println("A");
+    }else
+
+    {
+        System.out.println("wrong grade");
+    }
+}
