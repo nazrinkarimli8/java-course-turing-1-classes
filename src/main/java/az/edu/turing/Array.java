ed6a955774ab6167d84fec5c5d050bdad2344c9b
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-01-24 20:30:13 +0400

    first commit

---
+++ src/main/java/az/edu/turing/Array.java
@@ -0,0 +1,17 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class Array{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] * 2);
+        }
+    }
+}
