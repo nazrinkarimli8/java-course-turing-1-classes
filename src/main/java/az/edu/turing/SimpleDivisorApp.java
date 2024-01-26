c1f15f5e3a40054609f181bf97fef054bd1a5148
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-01-26 19:19:35 +0400

    Simple Divisor

---
+++ src/main/java/az/edu/turing/SimpleDivisorApp.java
@@ -0,0 +1,31 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class SimpleDivisorApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println("Divisors of the number:");
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("Odd divisors of the number:");
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0 && i % 2 == 1) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("Even divisors of the number:");
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0 && i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
+
