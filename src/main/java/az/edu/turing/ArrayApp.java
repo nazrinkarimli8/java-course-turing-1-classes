a34f855ae19db0269c70184a37d1acc7f20c9057
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-01-26 20:11:47 +0400

    Simple Divisor

---
+++ src/main/java/az/edu/turing/ArrayApp.java
@@ -0,0 +1,17 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class ArrayApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[][] = new int[n][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
