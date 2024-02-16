1dd7d77e3eb031ad197cc6d1d17ae5afe8aae8be
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-02-16 20:01:55 +0400

    SquareApp

---
+++ src/main/java/az/edu/turing/lesson14/SquareApp.java
@@ -0,0 +1,9 @@
+package az.edu.turing.lesson14;
+
+public class SquareApp {
+    public static void main(String[] args) {
+        final Square square1 = new Square(2);
+        final Square square2 = new Square(4);
+        final Square square3 = new Square(8);
+    }
+}
